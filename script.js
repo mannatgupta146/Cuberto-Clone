ab42c1fd08fae011645100e9b2b0115732a82941
mannatgupta146 <mannatgupta146@gmail.com>
2025-05-21 23:32:30 +0530

    added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 Shery.mouseFollower();
 Shery.makeMagnet(".magnet, .logo");
 Shery.hoverWithMediaCircle(".hvr",{videos: ["./a.mp4", "./b.mp4", "./c.mp4"]});
+
+gsap.to(".fleftelem",{
+    scrollTrigger:{
+        trigger: "#fimages",
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",   
+        endTrigger: ".last",
+        scrub: 1
+    },
+y: "-300%",
+ease: Power1
+})
